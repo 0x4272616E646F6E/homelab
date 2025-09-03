790db81f91a9a382bcc4ee3a5038322e49c10ae7
0x4272616E646F6E <59065614+0x4272616E646F6E@users.noreply.github.com>
2025-09-02 22:16:41 -0400

    updated flake
    Signed-off-by: 0x4272616E646F6E <59065614+0x4272616E646F6E@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Homelab Development Environment";
 
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; # stable channel
     flake-utils.url = "github:numtide/flake-utils";
@@ -18,13 +17,13 @@
         devShells.default = pkgs.mkShell {
           name = "homelab";
           NIX_SHELL_PRESERVE_PROMPT = 1;
+
           buildInputs = with pkgs; [
             # Core packages
             curl
             git
             vim
             wget
-            
 
             # Homelab packages
             nodePackages_latest.cdk8s-cli
@@ -32,14 +31,15 @@
             kubectl
             kustomize
             openjdk21
+            maven
             sops
-            talosctl 
+            talosctl
           ];
 
-          # Environment Variables
           shellHook = ''
             echo "Homelab Dev Environment 🚀"
             export KUBECONFIG=$PWD/kubeconfig
+            export JAVA_HOME=${pkgs.openjdk21}/lib/openjdk
           '';
         };
       });
